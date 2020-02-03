965047f107bcdd4d14c68c39bc5baad08463db3d
Anna Anttila <aanttila@f1r4s13.codam.nl>
2020-02-03 19:19:07 +0100

    utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,95 +5,102 @@
 /*                                                     +:+                    */
 /*   By: aanttila <aanttila@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/12/11 19:41:00 by aanttila       #+#    #+#                */
-/*   Updated: 2019/12/13 20:35:04 by aanttila      ########   odam.nl         */
+/*   Created: 2020/01/28 16:49:49 by aanttila       #+#    #+#                */
+/*   Updated: 2020/02/03 19:14:53 by aanttila      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strdup(char *s1)
+int		ft_strichr(char *s, char c)
 {
-	char	*dst;
-	int		i;
-	int		i2;
-
-	i = 0;
-	i2 = 0;
-	while (s1[i2] != '\0')
-	{
-		i2++;
-	}
-	i2++;
-	dst = (char *)malloc(i2 * sizeof(char));
-	if (dst == NULL)
-		return (0);
-	while (i < i2)
-	{
-		dst[i] = s1[i];
-		i++;
-	}
-	return (dst);
-}
-
-int		ft_strichr(char *s, int c)
-{
-	char	*str;
 	int		i;
 
 	i = 0;
-	str = (char *)s;
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (str[i] == (char)c)
+		if (s[i] == c)
 			return (i + 1);
 		i++;
 	}
-	if ((char)c == '\0')
+	if (c == '\0')
 		return (i + 1);
 	return (0);
 }
 
-char	*ft_strchr(char *s, int c)
+void	ft_bzero(void *s, size_t n)
 {
-	char	*str;
-	int		i;
+	size_t	i;
+	char	*s1;
 
 	i = 0;
-	str = (char *)s;
-	while (str[i] != '\0')
+	s1 = (char *)s;
+	while (i < n)
 	{
-		if (str[i] == (char)c)
-			return (&str[i]);
+		s1[i] = '\0';
 		i++;
 	}
-	if ((char)c == '\0')
-		return (&str[i]);
-	return (0);
 }
 
-char	*ft_strjoin(char *s1, char *s2, int k, int buf_size)
+size_t	ft_strlen(char *s)
+{
+	size_t	index;
+
+	index = 0;
+	if (!s)
+		return (1);
+	while (s[index] != '\0' && s[index] != '\n')
+		index++;
+	return (index);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*str;
 	int		i;
 	int		j;
 
-	str = (char *)malloc(((k * buf_size) * (buf_size + 1)) * sizeof(char));
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (str == 0)
 		return (0);
 	i = 0;
 	j = 0;
-	while (i < ((k * buf_size) - buf_size))
+	while (s1[i] != '\0' && s1[i] != '\n')
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	while (s2[j] != '\0')
+	while (s2[j] != '\0' && s2[j] != '\n')
 	{
 		str[i] = s2[j];
 		j++;
 		i++;
 	}
 	str[i] = '\0';
+	free(s1);
+	return (str);
+}
+
+char	*ft_strshift(char *str)
+{
+	int		i;
+	int		i2;
+
+	i = 0;
+	i2 = 0;
+	while (str[i] != '\n' && str[i] != '\0' && i <= (BUFFER_SIZE + 1))
+		i++;
+	i++;
+	while (str[i] != '\0')
+	{
+		str[i2] = str[i];
+		i++;
+		i2++;
+	}
+	while (i2 < BUFFER_SIZE + 1)
+	{
+		str[i2] = '\0';
+		i2++;
+	}
 	return (str);
 }
