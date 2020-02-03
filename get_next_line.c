f71ca9096e4c174d2e8d9382e7d0a78e1a9fdb5b
Anna Anttila <aanttila@f1r4s13.codam.nl>
2020-02-03 19:18:42 +0100

    gnl main file

--- get_next_line.c
+++ get_next_line.c
@@ -5,71 +5,79 @@
 /*                                                     +:+                    */
 /*   By: aanttila <aanttila@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/12/02 14:59:27 by aanttila       #+#    #+#                */
-/*   Updated: 2019/12/13 20:41:55 by aanttila      ########   odam.nl         */
+/*   Created: 2020/01/28 16:47:07 by aanttila       #+#    #+#                */
+/*   Updated: 2020/02/03 19:12:36 by aanttila      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-#define BUF_SIZE 5
-
-#include <strings.h>
-
-int		get_next_line(int fd, char **line)
+int			ft_error(char **line, char *str)
 {
-	int				ret;
-	static char		*buf;
-	char			str[BUF_SIZE + 1];
-	int i;
+	if (*line)
+		free(*line);
+	if (str)
+		ft_bzero(str, BUFFER_SIZE + 1);
+	line = NULL;
+	return (-1);
+}
 
-	i = 0;
-	fd = open("file.txt", O_RDONLY);
-	if (fd == -1)
-		return (1);
-	ret = 1;
-	while (ret != 0)
+char		*ft_linefill(char **line, char *str, int *retval)
+{
+	if (ft_strichr((str), '\n') == 0)
 	{
-		bzero(str, BUF_SIZE);
-		ret = read(fd, str, BUF_SIZE);
-		str[BUF_SIZE] = '\0';
-		printf("(%d) read[%d] : %s\n", ret, i, str);
-		i++;
-		buf = ft_strjoin(buf, str, i, BUF_SIZE);
-		printf("buf : %s\n", buf);
-		if (ft_strchr(buf, '\n') != 0)
-		{
-			printf("\nnew line found\n\n");
-			*line = ft_strdup(buf);
-			buf = buf + ft_strichr(buf, '\n');
-			return (*ft_strchr(*line, '\n'));
-		}
+		*line = ft_strjoin(*line, str);
+		if (*line == 0)
+			*retval = (ft_error(line, str));
+		ft_bzero(str, (BUFFER_SIZE + 1));
 	}
-//	if (close(fd) == -1)
-//		return (1);
-	return (0);
+	else if (ft_strichr(str, '\n') != 0)
+	{
+		*line = ft_strjoin(*line, str);
+		if (*line == 0)
+			*retval = (ft_error(line, str));
+		ft_strshift(str);
+		*retval = 1;
+	}
+	return (str);
 }
 
-int		main(void)
+char		*ft_read(char *str, int *retval, int ret, int fd)
 {
-	char *line;
-	get_next_line(1, &line);
-	get_next_line(1, &line);
-	return (0);
+	ft_bzero(str, (BUFFER_SIZE + 1));
+	ret = read(fd, str, BUFFER_SIZE);
+	if (ret < 0)
+		*retval = -1;
+	if (ret == 0)
+		*retval = 0;
+	return (str);
 }
 
+int			get_next_line(int fd, char **line)
+{
+	static char		str[BUFFER_SIZE + 1];
+	int				ret;
+	int				retval;
 
-
-
-/*
-char *str -> read(fd, str, bufsize)
-join my str + static buff -> so you save all the things 
-but so that you can also overwrite str everytime.
-search in buff for '\n' (with strchr)
-add everything up to that newline from buff into line
-
-strdup until newline into line;
-*line = ft_strcdup('\n', buff);
-buff = strchr('\n') + 1;
- */
+	retval = BUFFER_SIZE;
+	if (fd < 0 || line == NULL)
+		return (-1);
+	ret = 1;
+	*line = (char *)malloc(1 * sizeof(char));
+	if (*line == NULL)
+		return (-1);
+	*line[0] = '\0';
+	while (ret != 0 && retval != 0)
+	{
+		if (str[0] != '\0')
+			ft_linefill(line, str, &retval);
+		else
+			ft_read(str, &retval, ret, fd);
+		if (retval == -1 || retval == 1)
+			return (retval);
+	}
+	*line = ft_strjoin(*line, str);
+	if (*line == 0)
+		return (ft_error(line, str));
+	return (0);
+}
